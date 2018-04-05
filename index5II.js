7714229b1b500073c0c6535c6bcea524bafed453
llaksa <irvin.jair.pg@gmail.com>
2018-04-05 09:55:05 -0700

    let's round around

--- index5II.js
+++ index5II.js
@@ -1,10 +1,13 @@
-var five = require("johnny-five")
-var board = new five.Board()
+const fs = require('fs')
+const five = require("johnny-five")
+const board = new five.Board()
 
-var y0 = 0
+let y0 = 0
+let out
+let dir
 
 board.on("ready", function() {
-  var imu = new five.IMU({
+  let imu = new five.IMU({
     controller: "MPU6050"
     //freq: 100000      // optional
   });
@@ -12,9 +15,7 @@ board.on("ready", function() {
   imu.on("change", function() {
   //  console.log("=========================")
     y0 = 0.0549*this.gyro.yaw.angle + y0*0.945
-    //y0 = 0.0006281*this.gyro.yaw.angle + y0*0.9994
-    //console.log(" rotación horizontal          : ", this.gyro.yaw.angle)
-    //console.log(" rotación horizontal          : ", Math.round(y0*10)/10)
+    out = Math.round(y0*100)/100
   //  console.log("=========================")
   })
 
@@ -22,35 +23,69 @@ board.on("ready", function() {
      Motor A: PWM 9, dir 8
      Motor B: PWM 6, dir 5
    */
-  var motors = new five.Motors([
+  const motors = new five.Motors([
     { pins: { dir: 8, pwm: 9 }, invertPWM: true },
     { pins: { dir: 7, pwm: 6}, invertPWM: true }
   ])
 
   board.repl.inject({
     motors: motors,
-    myFwd: myFwd,
-    myRev: myRev
+    both: both,
+    delay: delay,
+    finalH: finalH,
+    finalA: finalA
   })
 
-  function myFwd (motor, value) {
-    // Go forward at full speed for 5 seconds
-    console.log("Full speed ahead!")
-    motors[motor].forward(value)
+  async function both (motorNum, pwmNum) {
+    motors[motorNum].fwd(pwmNum)
+    motors[1 - motorNum].rev(pwmNum)
+
+    let entry = pwmNum
+
+    if (dir == true) { // guardando datos según el sentido de giro
+      await fs.appendFile('outputPos.txt', `\n${out % 190}`, () => console.log(`Angular position: ${out % 190}`))
+      await fs.appendFile('entryPos.txt', `\n${entry}`, () => console.log(`PWM: ${entry}`) )
+    } else {
+      await fs.appendFile('outputNeg.txt', `\n${out % 190}`, () => console.log(`Angular position: ${out % 190}`))
+      await fs.appendFile('entryNeg.txt', `\n${entry}`, () => console.log(`PWM: ${entry}`) )
+    }
+
     board.wait(100, function () {
-      motors[motor].stop()
-      console.log(" rotación horizontal          : ", Math.round(y0*10)/10)
+      motors.stop()
     })
   }
 
-  function myRev (motor, value) {
-    // Go forward at full speed for 5 seconds
-    console.log("Full speed ahead!")
-    motors[motor].reverse(value)
-    board.wait(100, function () {
-      motors[motor].stop()
-      console.log(" rotación horizontal          : ", Math.round(y0*10)/10)
+  function delay () {
+    return new Promise(resolve => {
+      setTimeout(resolve, 300)
     })
+  }
+
+  async function finalH () {
+    await fs.unlink('outputPos.txt', function (err) {})
+    await fs.unlink('outputNeg.txt', function (err) {})
+    await fs.unlink('entryPos.txt', function (err) {})
+    await fs.unlink('entryNeg.txt', function (err) {})
+
+    for (let i = 0; i < 1500; i++) {
+      await dir = true
+      await both(0, 255)
+      await delay()
+    }
+
+  }
+
+  async function finalA () {
+    await fs.unlink('outputPos.txt', function (err) {})
+    await fs.unlink('outputNeg.txt', function (err) {})
+    await fs.unlink('entryPos.txt', function (err) {})
+    await fs.unlink('entryNeg.txt', function (err) {})
+
+    for (let i = 0; i < 1500; i++) {
+      await dir = false
+      await both(1, 255)
+      await delay()
+    }
   }
 
 })
