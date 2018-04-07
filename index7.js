1125a4eaa72a089bd99ca5215b7bf137958a5908
llaksa <irvin.jair.pg@gmail.com>
2018-04-07 15:52:37 -0700

    adding control part

--- index7.js
+++ index7.js
@@ -33,99 +33,19 @@ board.on("ready", function() {
 
   board.repl.inject({
     motors: motors,
-    both: both,
     delay: delay,
-    finalH: finalH,
-    finalA: finalA,
     lanzar: lanzar,
     grabar: grabar,
     grabarOne: grabarOne,
     todo: todo,
-    anti: anti
+    anti: anti,
+    control: control
   })
-
-  async function both (motorNum) {
-    let entry
-
-    if (i >= 500 && i < 1000) {
-      entry = 0
-      if (dir == true) { // guardando datos según el sentido de giro
-        await fs.appendFile('outN.txt', `\n${0}`, () => console.log(`Angular position: ${0}`))
-        await fs.appendFile('inpN.txt', `\n${0}`, () => console.log(`PWM: ${0}`) )
-      } else {
-        await fs.appendFile('outP.txt', `\n${0}`, () => console.log(`Angular position: ${0}`))
-        await fs.appendFile('inpP.txt', `\n${0}`, () => console.log(`PWM: ${0}`) )
-      }
-    } else if (i >= 1000 && i < 1500) {
-      if (i == 999) {
-        milqui = output
-      }
-
-      entry = 255
-      if (dir == true) { // guardando datos según el sentido de giro
-        await fs.appendFile('outN.txt', `\n${output}`, () => console.log(`Angular position: ${output}`))
-        await fs.appendFile('inpN.txt', `\n${255}`, () => console.log(`PWM: ${255}`) )
-      } else {
-        await fs.appendFile('outP.txt', `\n${output}`, () => console.log(`Angular position: ${output}`))
-        await fs.appendFile('inpP.txt', `\n${255}`, () => console.log(`PWM: ${255}`) )
-      }
-
-    } else if (i >= 1500 && i < 2000) {
-      entry = 0
-      if (dir == true) { // guardando datos según el sentido de giro
-        await fs.appendFile('outN.txt', `\n${milqui}`, () => console.log(`Angular position: ${milqui}`))
-        await fs.appendFile('inpN.txt', `\n${0}`, () => console.log(`PWM: ${0}`) )
-      } else {
-        await fs.appendFile('outP.txt', `\n${milqui}`, () => console.log(`Angular position: ${milqui}`))
-        await fs.appendFile('inpP.txt', `\n${0}`, () => console.log(`PWM: ${0}`) )
-      }
-    } else if (i >= 0 && i < 500) {
-      entry = 255
-      if (dir == true) { // guardando datos según el sentido de giro
-        await fs.appendFile('outN.txt', `\n${output}`, () => console.log(`Angular position: ${output}`))
-        await fs.appendFile('inpN.txt', `\n${255}`, () => console.log(`PWM: ${255}`) )
-      } else {
-        await fs.appendFile('outP.txt', `\n${output}`, () => console.log(`Angular position: ${output}`))
-        await fs.appendFile('inpP.txt', `\n${255}`, () => console.log(`PWM: ${255}`) )
-      }
-    }
-
-    motors[motorNum].fwd(entry)
-    motors[1 - motorNum].rev(entry)
-
-    board.wait(100, function () {
-      motors.stop()
-    })
-  }
 
   function delay (time) {
     return new Promise(resolve => {
       setTimeout(resolve, time)
     })
-  }
-
-  async function finalH () {
-    //await fs.unlink('outputPos.txt', function (err) {})
-    //await fs.unlink('entryPos.txt', function (err) {})
-
-    while (i < 2000) {
-      console.log(i)
-      dir = true
-      await both(0)
-      i++
-    }
-  }
-
-  async function finalA () {
-    //await fs.unlink('outputNeg.txt', function (err) {})
-    //await fs.unlink('entryNeg.txt', function (err) {})
-
-    while (i < 2000) {
-      console.log(i)
-      dir = false
-      await both(1)
-      i++
-    }
   }
 
   async function grabarOne () {
