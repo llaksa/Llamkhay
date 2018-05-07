4b167731be365c84a5b9242293a0a9aac561f4f5
llaksa <irvin.jair.pg@gmail.com>
2018-05-06 18:43:23 -0700

    improving code

--- temperatureControll.js
+++ temperatureControll.js
@@ -9,13 +9,13 @@ board.on("ready", function() {
   })
 
   let pwm
+  let y1 = 0
 
   this.pinMode(9, five.Pin.PWM)
-
-  var sensor = new five.Sensor("A1")
-  sensor.on("change", function() {
+  var potPin = new five.Sensor("A1")
+  potPin.on("change", function() {
     board.analogWrite(9, this.value / 4)
- })
+  })
 
   var temp = new five.Thermometer({
     controller: "LM35",
@@ -23,19 +23,18 @@ board.on("ready", function() {
     freq: 100
   })
 
-  this.repl.inject({
-    temp : temp,
-    relay : relay,
-    controll : controll
-  })
-
-  let y1 = 0
   temp.on("data", async function() {
     let y0 = this.celsius * 0.0609 + y1 * 0.9391
     let out = Math.round(y0)
     console.log(Math.round(out))
     y1 = y0
     //await controll(pwm)
+  })
+
+  this.repl.inject({
+    temp : temp,
+    relay : relay,
+    controll : controll
   })
 
   async function controll (x) {
