b1294088d65cfc4bd102e7be3439bf7c51fc3df9
llaksa <irvin.jair.pg@gmail.com>
2018-05-09 16:37:32 -0700

    improving code

---
+++ distanceControll.js
@@ -0,0 +1,64 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var proximity = new five.Proximity({
+    controller: "HCSR04",
+    pin: 7
+  });
+
+  proximity.on("data", function() {
+    console.log("Proximity: ");
+    console.log("  cm  : ", this.cm);
+    console.log("  in  : ", this.in);
+    console.log("-----------------");
+  });
+
+  proximity.on("change", function() {
+    console.log("The obstruction has moved.");
+  });
+
+  var motor;
+
+  motor = new five.Motor({
+    pins: {
+      pwm: 8,
+      dir: 9
+    }
+  });
+
+  board.repl.inject({
+    motor: motor
+  });
+
+  motor.on("start", function() {
+    console.log("start", Date.now());
+  });
+
+  motor.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  motor.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(50);
+    });
+  });
+
+  motor.on("reverse", function() {
+    console.log("reverse", Date.now());
+
+    // demonstrate stopping after 5 seconds
+    board.wait(5000, function() {
+      motor.stop();
+    });
+  });
+
+  // set the motor going forward full speed
+  motor.forward(255);
+
+});
+
